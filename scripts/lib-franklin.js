22afa9b9e8852a83a227bf6b83cc41ab86f4b17f
donyshchenko <donyshchenko@binaryanvil.com>
2023-11-07 00:48:01 +0200

    labs

---
+++ scripts/lib-franklin.js
@@ -0,0 +1,12 @@
+/**
+ * Retrieves the content of metadata tags.
+ * @param {string} name The metadata name (or property)
+ * @param doc Document object to query for the metadata. Defaults to the window's document
+ * @returns {string} The metadata value(s)
+ */
+
+export function getMetadata(name, doc = document) {
+    const attr = name && name.includes(':') ? 'property' : 'name';
+    const meta = [...doc.head.querySelectorAll(`meta[${attr}="${name}"]`)].map((m) => m.content).join(', ');
+    return meta || '';
+}
